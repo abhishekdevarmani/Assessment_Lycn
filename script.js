86e0bda0eb564e5b58ead003029b6a72e6fa72c4
Abhishek Devarmani <89991254+abhishekdevarmani@users.noreply.github.com>
2025-01-24 11:19:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function handleRedirection() {
     const feedback = document.querySelector('input[name="feedback"]:checked').value;
     if (feedback === "yes") {
         // Redirect to Google Review page (replace with actual URL)
-        window.location.href = "https://www.glassdoor.co.in/mz-survey/employer/collectReview_input.htm?i=3266107&j=true&y=&c=PAGE_INFOSITE_TOP";
+        window.location.href = "https://maps.app.goo.gl/YvJZNE5ermhRxMdY7";
     } else if (feedback === "no") {
         // Redirect to a Google Form (replace with your Google Form URL)
         window.location.href = "https://forms.gle/EdMrX1Us5TVmCz1u8";
